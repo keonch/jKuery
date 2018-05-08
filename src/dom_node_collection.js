fac4589ecc3cd4e52e124cfdd1c0a0f1b825d303
Keon Choi <keonch91@gmail.com>
2018-05-08 13:47:09 -0400

    Setup DOMNodeCollection class

---
+++ src/dom_node_collection.js
@@ -0,0 +1,5 @@
+export default class DOMNodeCollection {
+  constructor(HTMLelements) {
+    this.elements = HTMLelements;
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
+import DOMNodeCollection from './dom_node_collection';
+
 function $k(selector) {
   const nodeList = document.querySelectorAll(selector);
   const elements = Array.from(nodeList);
-  console.log(elements);
+  return new DOMNodeCollection(elements);
 }
 
 window.$k = $k;
