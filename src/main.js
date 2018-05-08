7cdef258e0099ae27787223b6ce61e6978e53a08
Keon Choi <keonch91@gmail.com>
2018-05-08 14:05:23 -0400

    Create selector comparison for core function

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 import DOMNodeCollection from './dom_node_collection';
 
 function $k(selector) {
-  const nodeList = document.querySelectorAll(selector);
-  const elements = Array.from(nodeList);
-  return new DOMNodeCollection(elements);
+  if (typeof selector === 'string') {
+    const nodeList = document.querySelectorAll(selector);
+    if (nodeList.length === 0) then throw "Could not find element in document";
+    return new DOMNodeCollection(Array.from(nodeList));
+
+  } else if (selector instanceof HTMLElement) {
+    return new DOMNodeCollection(selector);
+
+  } else {
+    throw "Argument is not a CSS selector nor an HTMLElement";
+  }
 }
 
 window.$k = $k;
