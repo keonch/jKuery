6c06232e409d9b903da1693a1c2cd033c4a9aaf5
Keon Choi <keonch91@gmail.com>
2018-05-08 14:56:00 -0400

    Create html and empty api

--- src/dom_node_collection.js
+++ src/dom_node_collection.js
@@ -2,4 +2,19 @@ export default class DOMNodeCollection {
   constructor(HTMLelements) {
     this.elements = HTMLelements;
   }
+
+  html(htmlString) {
+    if (typeof htmlString === 'undefined') {
+      return this.elements[0].innerHTML;
+    } else if (typeof htmlString === 'string') {
+      this.elements.forEach(node => node.innerHTML = htmlString);
+      return htmlString;
+    } else {
+      throw "Argument is not valid";
+    }
+  }
+
+  empty() {
+    this.html('');
+  }
 }
--- src/main.js
+++ src/main.js
@@ -1,16 +1,21 @@
 import DOMNodeCollection from './dom_node_collection';
 
 function $k(selector) {
-  if (typeof selector === 'string') {
-    const nodeList = document.querySelectorAll(selector);
-    if (nodeList.length === 0) then throw "Could not find element in document";
-    return new DOMNodeCollection(Array.from(nodeList));
+  switch (typeof selector) {
+    case "string":
+      const nodeList = document.querySelectorAll(selector);
+      if (nodeList.length === 0) throw "Could not find element in document";
+      return new DOMNodeCollection(Array.from(nodeList));
 
-  } else if (selector instanceof HTMLElement) {
-    return new DOMNodeCollection(selector);
+    case "object":
+      if (selector instanceof HTMLElement) {
+        return new DOMNodeCollection([selector]);
+      } else {
+        throw "Invalid argument"
+      }
 
-  } else {
-    throw "Argument is not a CSS selector nor an HTMLElement";
+    default:
+      throw "Invalid argument"
   }
 }
 
